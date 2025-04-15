fb8a67a2c3601604e1cddf52259bc591613b1aaa
jgreenwell <jsgreenwell83@gmail.com>
2025-04-15 16:49:35 -0500

    Added individual voting

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,28 @@ import React, {Fragment} from "react";
 
 // To use a fragment we'll 2 arrays
 const Profiles = [
-    {"img": adaLovelace, "blurb": "First Programmer", "id": 1, "sizechange": "normal"},
-    {"img": jonhall, "blurb": "Chair of Linux Board", "id": 2, "sizechange": "img-large"},
-    {"img": alanturing, "blurb": "Father of Modern Computing", "id": 3, "sizechange": "img-small"},
+    {img: adaLovelace, blurb: "First Programmer", name: "Ada", sizechange: "normal"},
+    {img: jonhall, blurb: "Chair of Linux Board", name: "Jon", sizechange: "img-large"},
+    {img: alanturing, blurb: "Father of Modern Computing", name: "Alan", sizechange: "img-small"},
 ]
 
 function Profile({ item }) {
-    // Final version before we make this a lambda (and use map)
-    // We don't need if when we can just set the value
+    // We need to setup a function to add votes but useState is easier
+    // This will setup an individual vote for each person
+    const [vote, setVote] = React.useState(0);
+
+    // We don't need if when we can just set the value so remove the if/else if/else
+    // We can also just Vote for a person so add the vote & state call (add 1 to vote)
     return (
         <div className="card">
             <img src={Profiles[item].img} alt="test" className={Profiles[item].sizechange} />
-            <p>{Profiles[item].blurb}</p>
+            <p>{Profiles[item].blurb}<br />
+                {vote}
+            </p>
+
+            <button onClick={() => setVote(vote+1)} type="button">
+                Vote for {Profiles[item].name}
+            </button>
         </div>
     )
 }
@@ -36,21 +46,9 @@ function AllItems() {
 }
 
 export default function App() {
-  const [mood, setMood] = React.useState("Meh");
-
   return (
     <>
       <h1>Profile</h1>
-      <p> Mood is {mood} </p>
-      <button onClick={() => setMood("Happy")}>
-          I'm Happy!
-      </button>
-      <button onClick={() => setMood("Sad")}>
-            Sssaaaddd
-      </button>
-        <button onClick={() => setMood("Excited")}>
-            Excited
-        </button>
         <AllItems />
     </>
   )
